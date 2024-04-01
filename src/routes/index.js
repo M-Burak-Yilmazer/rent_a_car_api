7d52ae8019a63afd04a5c4d79ceb6a2a9cca8d8a
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-04-01 16:16:36 +0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const router = require("express").Router();
+
+//auth
+router.use("/auth", require("./auth"));
+//user
+router.use("/users", require("./user"));
+//token
+router.use("/tokens", require("./token"));
+
+//order
+// router.use("/cars", require("./car"));
+//pizza
+// router.use("/reservations", require("./reservation"));
+
+module.exports = router;
